c7724c382560a2c5f31e1927a3015fcee171eb87
Robert <fishwickrob@live.com>
2018-05-28 18:48:26 +0100

    Tracks ball and frame number and increments appropriately

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -1,9 +1,26 @@
 describe('Game', function(){
-  var game = new Game;
-  describe('The games frame number', function(){
+    var game = new Game;
+    beforeEach(function() {
+      game.frameNumber = DEFAULT_FRAME_NUMBER;
+      game.ballNumber = DEFAULT_BALL_NUMBER;
+    });
+  describe('Frame number', function(){
     it('should return the default frame number for a new game', function() {
       expect(game.frameNumber).toEqual(DEFAULT_FRAME_NUMBER);
     });
+    it('should increment when the ball number max', function(){
+      for (var i = 1; i <= 2; i++) game.playBall();
+      expect(game.frameNumber).toEqual(2);
+    });
   });
-
+  describe('Ball', function(){
+    it('should increment the ball number', function() {
+      game.playBall();
+      expect(game.ballNumber).toEqual(MAX_BALL_NUMBER);
+    });
+    it('should reset ball number to default when frame changes', function() {
+      for (var i = 1; i <= 2; i++) game.playBall();
+      expect(game.ballNumber).toEqual(DEFAULT_BALL_NUMBER);
+    });
+  });
 });
--- src/game.js
+++ src/game.js
@@ -1,6 +1,19 @@
 const MAX_FRAME_NUMBER = 10;
-const DEFAULT_FRAME_NUMBER = 0;
+const MAX_BALL_NUMBER = 2;
+const DEFAULT_FRAME_NUMBER = 1;
+const DEFAULT_BALL_NUMBER = 1;
 
 var Game = function() {
   this.frameNumber = DEFAULT_FRAME_NUMBER;
+  this.ballNumber = DEFAULT_BALL_NUMBER;
+};
+
+Game.prototype.playBall = function() {
+  this.ballNumber += 1;
+  if (this.ballNumber > MAX_BALL_NUMBER){
+    this.frameNumber += 1;
+  };
+  if (this.ballNumber > MAX_BALL_NUMBER){
+    this.ballNumber = DEFAULT_BALL_NUMBER;
+  };
 };
